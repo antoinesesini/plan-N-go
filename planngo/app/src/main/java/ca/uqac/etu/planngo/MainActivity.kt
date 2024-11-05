0053c0484323053374536e88d69f8eb8ecba99e0
sesinian <antoine.sesini@etu.utc.fr>
2024-11-05 10:42:23 -0500

    Ajout du mode immersif

--- planngo/app/src/main/java/ca/uqac/etu/planngo/MainActivity.kt
+++ planngo/app/src/main/java/ca/uqac/etu/planngo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -41,6 +42,16 @@ class MainActivity : ComponentActivity() {
         requestPermission()
         getLastLocation()
 
+        // Activer le mode immersif pour masquer la barre de navigation
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                )
+
         setContent {
             AppTheme {
                 val navController = rememberNavController()
@@ -71,7 +82,24 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            // Réactiver le mode immersif quand l'activité regagne le focus
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    )
+        }
+    }
+
     private fun checkPermission():Boolean{
         //this function will return a boolean
         //true: if we have permission
